11af951ea1982918f64747d0a55195cb255dc640
Jeff Newman <jeff@driftless.xyz>
2024-04-22 21:56:13 -0500

    more limited testing

--- test/test_mtc.py
+++ test/test_mtc.py
@@ -147,40 +147,40 @@ def run_test_mtc(
     regress(extended, Path(out_dir))
 
 
-def test_mtc():
-    run_test_mtc(multiprocess=False)
-
-
-def test_mtc_chunkless():
-    run_test_mtc(multiprocess=False, chunkless=True)
-
-
-def test_mtc_mp():
-    run_test_mtc(multiprocess=True)
-
-
-def test_mtc_recode():
-    run_test_mtc(recode=True)
-
-
-def test_mtc_sharrow():
-    run_test_mtc(sharrow=True)
-
-
-def test_mtc_ext():
-    run_test_mtc(multiprocess=False, extended=True)
-
-
-def test_mtc_chunkless_ext():
-    run_test_mtc(multiprocess=False, chunkless=True, extended=True)
-
-
-def test_mtc_mp_ext():
-    run_test_mtc(multiprocess=True, extended=True)
-
-
-def test_mtc_sharrow_ext():
-    run_test_mtc(sharrow=True, extended=True)
+# def test_mtc():
+#     run_test_mtc(multiprocess=False)
+#
+#
+# def test_mtc_chunkless():
+#     run_test_mtc(multiprocess=False, chunkless=True)
+#
+#
+# def test_mtc_mp():
+#     run_test_mtc(multiprocess=True)
+#
+#
+# def test_mtc_recode():
+#     run_test_mtc(recode=True)
+#
+#
+# def test_mtc_sharrow():
+#     run_test_mtc(sharrow=True)
+#
+#
+# def test_mtc_ext():
+#     run_test_mtc(multiprocess=False, extended=True)
+#
+#
+# def test_mtc_chunkless_ext():
+#     run_test_mtc(multiprocess=False, chunkless=True, extended=True)
+#
+#
+# def test_mtc_mp_ext():
+#     run_test_mtc(multiprocess=True, extended=True)
+#
+#
+# def test_mtc_sharrow_ext():
+#     run_test_mtc(sharrow=True, extended=True)
 
 
 EXPECTED_MODELS = [
@@ -319,14 +319,3 @@ def test_mtc_extended_progressive(chunk_training_mode, recode_pipeline_columns):
 
 
 if __name__ == "__main__":
-    run_test_mtc(multiprocess=False)
-    run_test_mtc(multiprocess=True)
-    run_test_mtc(multiprocess=False, chunkless=True)
-    run_test_mtc(recode=True)
-    run_test_mtc(sharrow=True)
-    run_test_mtc(multiprocess=False, extended=True)
-    run_test_mtc(multiprocess=True, extended=True)
-    run_test_mtc(multiprocess=False, chunkless=True, extended=True)
-    run_test_mtc(recode=True, extended=True)
-    run_test_mtc(sharrow=True, extended=True)
-    test_mtc_extended_progressive()
